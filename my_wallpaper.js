fe95d5604f9459c550f4fa87655941406141a2bc
pyperjennings <jenninpype@myvuw.ac.nz>
2024-07-25 11:58:41 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,7 +4,7 @@ let rect_height = 20;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -19,26 +19,32 @@ function wallpaper_background() {
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  fill(207, 200, 198)
+  fill(0, 0, 0); //black 
   triangle(60, 100, 
     135, 100, 
      60, 50);
 triangle(100, 70, 
       140, 100, 
        140, 50);
-fill(207, 200, 198)
+fill(255, 255, 255); //black
 ellipse(100, 100, 80, 80);
-fill(123, 175, 179)
-ellipse(80, 100, 10, 10);
-ellipse(120, 100, 10, 10);
+fill(255, 255, 255) //white
+ellipse(80, 100, 15, 15);
+ellipse(120, 100, 15, 15);
+fill(0, 0, 0); //black
+ellipse(80, 100, 9, 9);
+ellipse(120, 100, 9, 9);
+fill(250, 175, 241) //pink
+triangle(100, 100, 
+  105, 110, 
+   95, 110);
+strokeWeight(1);
 line(70, 110, 45, 95);
 line(70, 110, 45, 105);
 line(70, 110, 45, 115);
 line(130, 110, 155, 115);
 line(130, 110, 155, 105);
 line(130, 110, 155, 95);
-fill(250, 175, 241)
-triangle(100, 100, 
-        105, 110, 
-         95, 110);
+
+
 }
