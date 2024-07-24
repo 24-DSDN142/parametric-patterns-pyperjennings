38fb5ec8bc2c3b1005a3a8a7794d9eab8b5e19d3
pyperjennings <jenninpype@myvuw.ac.nz>
2024-07-25 11:18:09 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,7 +4,7 @@ let rect_height = 20;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -15,9 +15,30 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background(247, 244, 210); //light honeydew green colour
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(30 ,30, rect_width, rect_height);
+  fill(207, 200, 198)
+  triangle(60, 100, 
+    135, 100, 
+     60, 50);
+triangle(100, 70, 
+      140, 100, 
+       140, 50);
+fill(207, 200, 198)
+ellipse(100, 100, 80, 80);
+fill(123, 175, 179)
+ellipse(80, 100, 10, 10);
+ellipse(120, 100, 10, 10);
+line(70, 110, 45, 95);
+line(70, 110, 45, 105);
+line(70, 110, 45, 115);
+line(130, 110, 155, 115);
+line(130, 110, 155, 105);
+line(130, 110, 155, 95);
+fill(250, 175, 241)
+triangle(100, 100, 
+        105, 110, 
+         95, 110);
 }
