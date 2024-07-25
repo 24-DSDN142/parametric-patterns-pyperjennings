30801450141eb9193529667a55cb6b0627e1c8db
pyperjennings <jenninpype@myvuw.ac.nz>
2024-07-25 13:18:30 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -21,6 +21,7 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
+// ears
   fill(0, 0, 0); //black 
   triangle(60, 100, 
     135, 100, 
@@ -28,30 +29,34 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 triangle(100, 70, 
       140, 100, 
        140, 50);
-
-fill(255, 255, 255); //black
+// head
+fill(255, 255, 255); //white
 ellipse(100, 100, 80, 80);
-fill(0) //white
-arc(100, 100, 80, 80, 262, 50, CHORD);
-arc(100, 100, 80, 80, 125, 270, CHORD);
-fill(255, 255, 255) //white
-ellipse(80, 100, 15, 15);
+// black part of face
+fill(0) //black
+arc(100, 100, 80, 80, 280, 50, CHORD);
+arc(100, 100, 80, 80, 125, 260, CHORD);
+// outside of eyes
+fill(255, 255, 255); //white
+ellipse(82, 100, 15, 15);
 ellipse(120, 100, 15, 15);
+//inside of eyes
 fill(0, 0, 0); //black
-ellipse(80, 100, 9, 9);
+ellipse(82, 100, 9, 9);
 ellipse(120, 100, 9, 9);
+// nose
+strokeWeight(0);
 fill(250, 175, 241) //pink
-triangle(100, 100, 
-  105, 110, 
-   95, 110);
-strokeWeight(2);
+ellipse(100, 108, 12, 6);
+// whiskers 
+strokeWeight(1);
 line(70, 110, 45, 95);
 line(70, 110, 45, 105);
 line(70, 110, 45, 115);
 line(130, 110, 155, 115);
 line(130, 110, 155, 105);
 line(130, 110, 155, 95);
-
+fill(0)
 
 }
 
