079c860574d031657441cca3bdce1451a7304a3b
pyperjennings <jenninpype@myvuw.ac.nz>
2024-08-01 12:09:30 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,10 +1,15 @@
 //your parameter variables go here!
-// let rect_width  = 20;//var 1
-// let rect_height = 20; //var 2
+//var 1
+let eyeshape = 5 
+//var 2
+let headcolour = 3; 
 //var 3
+let whisker = 100;
 //var 4
-let facePattern = 1; //var 5
-
+let pawpad = 22;
+let pawfingers = 10;
+//var 5
+let facePattern = 101; 
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(DEVELOP_GLYPH);
@@ -40,31 +45,41 @@ triangle(110, 70,
          135, 100, 
          140, 50);
 // head
-fill(255, 255, 255); //white
+if(headcolour==1){
+  fill(0); 
+}
+else if(headcolour==2){
+  fill(230, 175, 103);
+}
+else if(headcolour==3){
+}
+else {
+  fill(255)
+}
 ellipse(100, 100, 80, 80);
 // black part of face
-fill(0) //black
+fill(0) //
 arc(100, 100, 80, 80, 272, 35, CHORD);//
 // arc(100, 100, 80, 80, 125, 270, CHORD);
-
 arc(100, 100, 80, 80, 142, 267, CHORD);//
 // arc(100, 100, 80, 80, 25, 270, CHORD);
-
-arc(142, 77, 85, 90, 105, 202, CHORD);
-
-arc(58, 77, 85, 90, -22, 73, CHORD);
-
+arc(143, 77, 87, 90, 105, 202, CHORD);
+//my if statment
+if( facePattern > 100){
+arc(56, 77, 87, 95, -22, 75, CHORD);
+} 
 // fill(255, 0, 0);
 // ellipse(142, 77, 20, 20);
 fill(255);
+
 // outside of eyes
 fill(255, 255, 181); //greeny yellow
 ellipse(82, 100, 15, 15);
 ellipse(120, 100, 15, 15);
 //inside of eyes
 fill(0, 0, 0); //black
-ellipse(82, 100, 5, 13);
-ellipse(120, 100, 5, 13);
+ellipse(82, 100, eyeshape, 13);
+ellipse(120, 100, eyeshape, 13);
 // nose
 strokeWeight(1);
 line(100, 110, 
@@ -83,21 +98,25 @@ line(70, 110, 45, 115);
 line(130, 110, 155, 115);
 line(130, 110, 155, 105);
 line(130, 110, 155, 95);
-
+if(whisker > 100){
+line(130, 110, 155, 125);
+line(70, 110, 45, 125);
+line(130, 110, 155, 85);
+line(70, 110, 45, 85);
+}
 //paw top 
 fill(0)
-ellipse(40, 40, 22, 22);
-ellipse(23, 33, 10, 10);
-ellipse(32, 23, 10, 10);
-ellipse(47, 23, 10, 10);
-ellipse(57, 33, 10, 10);
+ellipse(40, 40, pawpad, pawpad);
+ellipse(23, 33, pawfingers, pawfingers);
+ellipse(32, 23, pawfingers, pawfingers);
+ellipse(47, 23, pawfingers, pawfingers);
+ellipse(57, 33, pawfingers, pawfingers);
 //paw bottom
-ellipse(160, 170, 22, 22);
-ellipse(143, 163, 10, 10);
-ellipse(177, 163, 10, 10);
-ellipse(152, 152, 10, 10);
-ellipse(167, 152, 10, 10);
-
+ellipse(160, 170, pawpad, pawpad);
+ellipse(143, 163, pawfingers, pawfingers);
+ellipse(177, 163, pawfingers, pawfingers);
+ellipse(152, 152, pawfingers, pawfingers);
+ellipse(167, 152, pawfingers, pawfingers);
 
 }
 
